c42f176d62fc3562d3ef62d370f7b60bbca5019d
DrOppenheimer <kevin.p.keegan@gmail.com>
2015-11-23 09:41:16 -0600

    debug

--- download_and_stats_FASTQ.sh
+++ download_and_stats_FASTQ.sh
@@ -12,7 +12,8 @@
 # R script combine_docker_outputs.r
 
 # variables
-my_list="";
+#my_list="";
+my_list="err_list_1_of_4.11-18-15.txt.test"; # list with two samples for testing (4 FASTQ)
 my_fastq_log=$my_list.FASTQ_log.txt;
 my_tar_log=$my_list.tar_log.txt;
 my_run_log=$my_list.run_log.txt;
