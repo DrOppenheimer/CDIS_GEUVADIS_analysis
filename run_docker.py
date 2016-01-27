ecd3aaa4342a2d6aa618e86dfb5f0103246e2a5f
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-27 17:36:13 -0600

    debug run_docker.p[y

--- run_docker.py
+++ run_docker.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python
 import os
-import argparse
 
-#import boto
-#import boto.s3.connection
-#import os
-#import re
-#import json
-#import os.path
-#from os.path import basename
-#from pprint import pprint
-
-#print("example file: \"ERR_tar.12Mb.gz\"")
-
-# Added some argument parsing so it's easier to wrap this script with others.
-parser = argparse.ArgumentParser(description='Script to run Stuti\'s RNASeq processing docker')
-parser.add_argument('-f','--list_file', help='list_of_files_to_process (*.fastq.tar.gz, a zip of mate pair fastq files)', required=True, default="filenames_1.txt")
-parser.add_argument('-d','--docker_id', help='id of the docker image to use', required=False, default="0ede86ece3ce")
-parser.add_argument('-o','--output_dir', help='directory path for output', required=False, default="/home/ubuntu/SCRATCH/geuvadis_results/")
-args = parser.parse_args()
-
-list_file = args.list_file
-docker_id = args.docker_id
-output_dir = args.output_dir
-
-#docker_id = "0ede86ece3ce"
-#f = open("filenames_1.txt", "r")
-f = open(list_file, "r")
-#docker_id = docker_id
+docker_id = "0ede86ece3ce"
+f = open("filenames_1.txt", "r")
 for filename in f:
     filename = filename.rstrip()
     analysis_id = filename.replace(".fastq.tar.gz", "")
-    filepath = output_dir+"/%s" %(filename)
-    print "Processing "+analysis_id
-    #output = "/home/ubuntu/SCRATCH/geuvadis_results/%s" %(analysis_id)
-    output = output_dir+"/%s" %(analysis_id)
+    filepath = "/home/ubuntu/SCRATCH/%s" %(filename)
+    print analysis_id
+    output = "/home/ubuntu/SCRATCH/geuvadis_results/%s" %(analysis_id)
     if not os.path.isdir(output):
 	os.mkdir(output)
 
