fd3adbda33f8f08f4434156bfbbd8a7b56d37d43
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-28 09:34:23 -0600

    update scripts

--- run_docker.py
+++ run_docker.py
@@ -12,7 +12,24 @@ for filename in f:
     if not os.path.isdir(output):
 	os.mkdir(output)
 
-    os.system("docker run -v /home/ubuntu/:/host/home -v /etc:/host/etc -v /mnt/SCRATCH:/home/ubuntu/SCRATCH/ -i -t %s /usr/bin/python /home/ubuntu/expression/pipeline_elastic_cluster_new.py --analysis_id %s --gtf /home/ubuntu/SCRATCH/geuvadis_genome/gencode.v19.annotation.hs37d5_chr.gtf --input_file %s --p 8 --star_pipeline /home/ubuntu/expression/icgc_rnaseq_align/star_align.py --output_dir %s --genome_fasta_file /home/ubuntu/SCRATCH/geuvadis_genome/hs37d5.fa  --genome_dir /home/ubuntu/SCRATCH/geuvadis_genome/star_genome/ --quantMode TranscriptomeSAM --cufflinks_pipeline /home/ubuntu/expression/compute_expression.py --ref_flat /home/ubuntu/SCRATCH/geuvadis_genome/refFlat.txt" %(docker_id, analysis_id, filepath, output))
+    os.system("docker run
+    -v /home/ubuntu/:/host/home
+    -v /etc:/host/etc
+    -v /mnt/SCRATCH:/home/ubuntu/SCRATCH/
+    -i
+    -t %s /usr/bin/python /home/ubuntu/expression/pipeline_elastic_cluster_new.py
+    --analysis_id %s
+    --gtf /home/ubuntu/SCRATCH/geuvadis_genome/gencode.v19.annotation.hs37d5_chr.gtf
+    --input_file %s
+    --p 8
+    --star_pipeline /home/ubuntu/expression/icgc_rnaseq_align/star_align.py
+    --output_dir %s
+    --genome_fasta_file /home/ubuntu/SCRATCH/geuvadis_genome/hs37d5.fa
+    --genome_dir /home/ubuntu/SCRATCH/geuvadis_genome/star_genome/
+    --quantMode TranscriptomeSAM
+    --cufflinks_pipeline /home/ubuntu/expression/compute_expression.py
+    --ref_flat /home/ubuntu/SCRATCH/geuvadis_genome/refFlat.txt"
+    %(docker_id, analysis_id, filepath, output))
 
 
 
